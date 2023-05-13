0451c28e360e40cd7571325504bd1c13f2c0efe7
Rashmidias19 <rashmidias819@gmail.com>
2023-05-13 13:06:11 +0530

    commit 2

---
+++ site/index.js
@@ -0,0 +1,18 @@
+let hour=18;
+
+if(6<=hour && hour<=12){
+    console.log("Good morning");
+}
+else if(12<hour && hour<=18){
+    console.log("Good Afternoon");
+}
+else{
+    console.log("Good Evening");
+}
+
+for(let i=0;i<5;i++){
+    console.log("Hello World",i,i+1);
+}
+
+let i=0;
+while
